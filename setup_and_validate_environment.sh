0c751f5fe2ae6ad164dc1115e826112866ac28a6
Femure <maxime.femery@gmail.com>
2024-10-27 00:24:12 +0200

    updates

--- setup_and_validate_environment.sh
+++ setup_and_validate_environment.sh
@@ -8,6 +8,12 @@ set -e  # Exit on error
 openssl req -x509 -newkey rsa:4096 -keyout ca/ca_certs/key.pem -out ca/ca_certs/cert.pem \
     -days 365 -passout pass:"third-wheel" -subj "/C=US/ST=private/L=province/O=city/CN=hostname.example.com" &>/dev/null
 
+# Check if the certificates were created
+if [[ ! -f ca/ca_certs/key.pem || ! -f ca/ca_certs/cert.pem ]]; then
+    echo "Failed to generate self-signed certificate."
+    exit 1
+fi
+
 # Sign a certificate for the server
 cargo run --example sign_cert_for_site -- my_test_site.com -o ca/simple_server/localhost.pem -p third-wheel &>/dev/null
 
@@ -19,11 +25,16 @@ cat ca/ca_certs/key.pem >> ca/simple_server/localhost.pem
 echo "Waiting for the server to start..."
 sleep 1
 
+# Verify if the server started correctly
+if ! (curl -s --cacert ./ca/ca_certs/cert.pem --resolve my_test_site.com:4443:127.0.0.1 https://my_test_site.com:4443 -o /dev/null); then
+    echo "Server did not start correctly."
+    exit 1
+fi
+
 # Define expected output and save it to a file
 echo -n "<html><head><title>Environment Test</title></head></html>" > /tmp/curl_test
 
 # Test server output with curl
-# Fetch server output without extra info and save to 'curl_output_test'
 curl -s --cacert ./ca/ca_certs/cert.pem --resolve my_test_site.com:4443:127.0.0.1 https://my_test_site.com:4443 \
     -o /tmp/curl_output_test
 
@@ -33,4 +44,5 @@ if diff -u /tmp/curl_test /tmp/curl_output_test; then
     echo "Everything worked, your environment is looking good"
 else
     echo "Curl received unexpected output, something is wrong"
+    exit 1
 fi
